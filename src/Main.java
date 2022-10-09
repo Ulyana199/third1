0969f5f27ffa7eb22b647ba969f902cba1ce9a78
Ульяна <basalaeva.u.i>
2022-10-09 23:06:48 +0700

    commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 20_000;
+
+        int bonus = ticketPrice / 20;
+        System.out.println("Бонусные милли:" + bonus);
+    }
+}
